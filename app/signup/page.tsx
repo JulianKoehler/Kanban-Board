3502210096ac379f9f48c7e93a7cf8fa17d3d9a4
Julian Köhler <julkoehl@icloud.com>
2024-01-07 15:58:17 +0100

    Fixed App Routing issues

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -12,7 +12,7 @@ import { UserReturn } from '@/types/data/user';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React, { FormEvent, useRef, useEffect } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
 
